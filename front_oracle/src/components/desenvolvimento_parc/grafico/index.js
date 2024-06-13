04de646dfcbfaaf742cb9304537647960672deeb
Diane Alves <102235722+Diane-Moreno@users.noreply.github.com>
2024-06-13 19:01:11 -0300

    ✨ feat: Conexão desenvolvimento de parceiro
    Conexão frontend e backend de desenvolvimento de parceiro

--- front_oracle/src/components/desenvolvimento_parc/grafico/index.js
+++ front_oracle/src/components/desenvolvimento_parc/grafico/index.js
@@ -1,60 +1,119 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, ActivityIndicator } from "react-native";
 import { BarChart } from "react-native-chart-kit";
+import { useFocusEffect } from "@react-navigation/native";
+import Connection from "../../../connection";
 import stylesGrafico from "./desenvolvimento_parc.styles";
 
-export function Grafico() {
-    const data = {
-        labels: ["Build", "Service", "Hardware", "Sell"],
-        datasets: [
-            {
-                data: [3, 4, 8, 5],
-            },
-        ],
+export function Grafico({ filtroTrackSelecionado }) {
+    const conn = Connection();
+    const [chartData, setChartData] = useState(null);
+
+    // Função para buscar os dados do endpoint
+    const fetchData = async () => {
+        try {
+            // Faz uma solicitação para o endpoint para obter os dados
+            const response = await conn.get("/tracksById/:user_id");
+            // Extrai os dados da resposta
+            const responseData = response.data;
+
+            console.log("Dados obtidos:", responseData);
+
+            // Função para mapear os nomes das tracks como abreviações
+            const getShortLabel = (label) => {
+                switch (label) {
+                    case "Cloud Sell Track":
+                        return "Sell";
+                    case "Cloud Service Track":
+                        return "Service";
+                    case "License & Hardware Track":
+                        return "L&H";
+                    case "Cloud Build Track":
+                        return "Build";
+                    default:
+                        return label;
+                }
+            };
+
+            // Mapeia os dados para formatá-los corretamente para o gráfico
+            const labels = responseData.map((item) =>
+                getShortLabel(item["Track Name"])
+            );
+            console.log("Labels:", labels);
+            const dataValues = responseData.map(
+                (item) => item["Completions Count"]
+            );
+            console.log("Data values:", dataValues);
+
+            // Define os dados do gráfico
+            const data = {
+                labels: labels,
+                datasets: [
+                    {
+                        data: dataValues,
+                    },
+                ],
+            };
+
+            // Define os dados do gráfico com os dados obtidos
+            setChartData(data);
+        } catch (error) {
+            console.error("Erro ao buscar os dados:", error);
+        }
     };
 
-    const barColors = ['#C74634', '#C74634', '#C74634', '#C74634'];
+    useFocusEffect(
+        React.useCallback(() => {
+            fetchData();
+        }, [])
+    );
+
+    const barColors = ["#C74634", "#C74634", "#C74634", "#C74634"];
 
     return (
         <View style={stylesGrafico.container}>
-            <BarChart
-                data={data}
-                width={300}
-                height={200}
-                chartConfig={{
-                    backgroundGradientFrom: "#F8F8F8",
-                    backgroundGradientTo: "#F8F8F8",
-                    color: () => "#C74634",
-                    labelColor: () => "#B1B1B1",
-                    propsForBackgroundLines: {
-                        strokeWidth: 0,
-                    },
-                    decimalPlaces: 0,
-                    style: {
-                        borderRadius: 20,
-                    },
-                }}
-                fromZero={true}
-                style={{
-                    marginVertical: 8,
-                    marginLeft: 0,
-                }}
-                yAxisSuffix=""
-                renderDotContent={({ x, y, index }) => (
-                    <View
-                        key={index}
-                        style={{
-                            position: "absolute",
-                            left: x - 8,
-                            top: y - 20,
-                            width: 16,
-                            height: 16,
-                            borderRadius: 8,
-                            backgroundColor: barColors[index],
-                        }}
-                    />
-                )}
-            />
+            {chartData ? (
+                <BarChart
+                    data={chartData}
+                    width={300}
+                    height={200}
+                    chartConfig={{
+                        backgroundGradientFrom: "#F8F8F8",
+                        backgroundGradientTo: "#F8F8F8",
+                        color: () => "#C74634",
+                        labelColor: () => "#B1B1B1",
+                        propsForBackgroundLines: {
+                            strokeWidth: 0,
+                        },
+                        decimalPlaces: 0,
+                        style: {
+                            borderRadius: 20,
+                        },
+                    }}
+                    fromZero={true}
+                    style={{
+                        marginVertical: 8,
+                        marginLeft: 0,
+                    }}
+                    yAxisSuffix=""
+                    renderDotContent={({ x, y, index }) => (
+                        <View
+                            key={index}
+                            style={{
+                                position: "absolute",
+                                left: x - 8,
+                                top: y - 20,
+                                width: 16,
+                                height: 16,
+                                borderRadius: 8,
+                                backgroundColor: barColors[index],
+                            }}
+                        />
+                    )}
+                />
+            ) : (
+                <ActivityIndicator size="large" color="#0000ff" />
+            )}
         </View>
     );
 }
--- front_oracle/src/components/desenvolvimento_parc/relatorio/index.js
+++ front_oracle/src/components/desenvolvimento_parc/relatorio/index.js
@@ -1,56 +1,102 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, ScrollView, Text } from 'react-native';
-import stylesRelatorio from './relatorio.styles';
+import React, { useEffect, useState } from "react";
+import { View, ScrollView, Text, Alert } from "react-native";
+import Connection from "../../../connection";
+import XLSX from 'xlsx';
+import * as FileSystem from 'expo-file-system';
+import * as Sharing from 'expo-sharing';
+import stylesRelatorio from "./relatorio.styles";
 
-export class Relatorio extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tableData: [
-        { user: 'Usuário 1', email: 'usuario1@email.com', product: 'Hardware' },
-        { user: 'Usuário 2', email: 'usuario2@email.com', product: 'Service' },
-        { user: 'Usuário 3', email: 'usuario3@email.com', product: 'Build' },
-        { user: 'Usuário 4', email: 'usuario4@email.com', product: 'Sell' }
-      ]
-    }
-  }
+export function Relatorio({ filtroTrackSelecionado }) {
+    const conn = Connection();
+    const [tableData, setTableData] = useState([]);
 
-  render() {
-    const { tableData } = this.state;
+    useEffect(() => {
+        fetchData();
+    }, [filtroTrackSelecionado]);
+
+    const fetchData = async () => {
+        try {
+            const response = await conn.get("/tracksById/:user_id");
+            const responseData = response.data;
+            setTableData(responseData);
+        } catch (error) {
+            console.error("Erro ao buscar dados:", error);
+        }
+    };
+
+    const exportToExcel = async () => {
+        if (tableData.length === 0) {
+            Alert.alert("Nenhum dado para exportar.");
+            return;
+        }
+
+        const wsData = [
+            ["Track", "Conclusões"],
+            ...tableData.map(row => [
+                row["Track Name"],
+                row["Completions Count"]
+            ])
+        ];
+        const ws = XLSX.utils.aoa_to_sheet(wsData);
+        const wb = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, "Relatório");
+
+        const wbout = XLSX.write(wb, { type: 'base64', bookType: "xlsx" });
+
+        // Diretório para salvar o arquivo temporariamente
+        const directory = `${FileSystem.documentDirectory}excel_files/`;
+
+        try {
+            // Criar o diretório se ele não existir
+            await FileSystem.makeDirectoryAsync(directory, { intermediates: true });
+            
+            // Caminho do arquivo
+            const filePath = `${directory}relatorio.xlsx`;
+            
+            // Escrever o arquivo no sistema de arquivos
+            await FileSystem.writeAsStringAsync(filePath, wbout, {
+                encoding: FileSystem.EncodingType.Base64,
+            });
+
+            // Abrir o compartilhamento para compartilhar e baixar o arquivo
+            await Sharing.shareAsync(filePath, { mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+            
+        } catch (error) {
+            console.error("Erro ao exportar para Excel:", error);
+            Alert.alert("Erro ao exportar para Excel.");
+        }
+    };
+
     return (
-      <View>
-        <View style={stylesRelatorio.container}>
-            <ScrollView>
-            <View style={stylesRelatorio.row}>
-                <View style={[stylesRelatorio.column, { flex: 2 }]}>
-                <Text style={stylesRelatorio.title}>Parceiro</Text>
-                {tableData.map((rowData, index) => (
-                    <View key={index} style={stylesRelatorio.row}>
-                    <View style={stylesRelatorio.cell}>
-                        <Text style={stylesRelatorio.text}>{rowData.user}</Text>
-                    </View>
-                    <View style={stylesRelatorio.cell}>
-                        <Text style={stylesRelatorio.text}>{rowData.email}</Text>
-                    </View>
-                    </View>
-                ))}
-                </View>
-                <View style={[stylesRelatorio.column, stylesRelatorio.alignRight]}>
-                    <Text style={stylesRelatorio.title}>Produto</Text>
-                    {tableData.map((rowData, index) => (
-                        <View key={index} style={stylesRelatorio.row}>
-                            <View style={stylesRelatorio.cell}>
-                                <Text style={stylesRelatorio.textProduct}>{rowData.product}</Text>
-                            </View>
+        <View>
+            <View style={stylesRelatorio.container}>
+                <ScrollView horizontal={true}>
+                    <View style={stylesRelatorio.row}>
+                        <View style={stylesRelatorio.column}>
+                            <Text style={stylesRelatorio.title}>Track</Text>
+                            {tableData.map((rowData, index) => (
+                                <View key={index} style={stylesRelatorio.row}>
+                                    <Text style={stylesRelatorio.text}>
+                                        {rowData["Track Name"]}
+                                    </Text>
+                                </View>
+                            ))}
                         </View>
-                    ))}
-                </View>
+                        <View style={stylesRelatorio.column}>
+                            <Text style={stylesRelatorio.title}>Conclusões</Text>
+                            {tableData.map((rowData, index) => (
+                                <View key={index} style={[stylesRelatorio.row, { paddingLeft: 50 }]}>
+                                    <Text style={stylesRelatorio.text}>
+                                        {rowData["Completions Count"]}
+                                    </Text>
+                                </View>                            
+                            ))}
+                        </View>
+                    </View>
+                </ScrollView>
             </View>
-            </ScrollView>
         </View>
-      </View>
-    )
-  }
+    );
 }
 
 export default Relatorio;
